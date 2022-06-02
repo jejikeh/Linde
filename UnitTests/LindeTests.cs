863b462e9ee01b4dffce5d26003aa609803697a0
jejikeh <jejikeh@gmail.com>
2022-06-03 01:24:01 +0300

    wait, unit tests !? 😮

---
+++ UnitTests/LindeTests.cs
@@ -0,0 +1,20 @@
+using Linde;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LindeTests
+    {
+        [TestMethod]
+        public void AlgaeMethod()
+        {
+            List<StringBuilder> expectedOutputs = new List<StringBuilder>()
+            {
+                new StringBuilder(""),
+                new StringBuilder(""),
+                new StringBuilder(""),
+                new StringBuilder(""),
+            };
+        }
+    }
+}
